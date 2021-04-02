3d124c471a07259cfb5b01d5924a8b4158730d2a
Sebastian Sikora <ssikora80@gmail.com>
2021-04-02 14:39:29 +0200

    Add Hero and List components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import App from './components/App/App'; // komponent (zawartość) strony
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app')); // znajdź element (w index.html), którego id to 'app', i wstaw do niego komponent App (ściezka powyżej)
 
