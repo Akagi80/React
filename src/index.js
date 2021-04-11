294a5623ffd26b1b79f3f77928ad6b8039f44c1d
Sebastian Sikora <ssikora80@gmail.com>
2021-04-11 14:35:10 +0200

    Change Columns and Lists to Redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
-import App from './components/App/App'; // komponent (zawartość) strony
+import App from './components/App/AppContainer'; // komponent (zawartość) strony
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('app')); // znajdź element (w index.html), którego id to 'app', i wstaw do niego komponent App (ściezka powyżej)
+// ReactDOM.render(<App />, document.getElementById('app')); // znajdź element (w index.html), którego id to 'app', i wstaw do niego komponent App (ściezka powyżej)
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
 
